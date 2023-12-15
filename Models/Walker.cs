0caca1c3c011dc755f6d60d36ecbbe83d699d8f0
Ava Dominie <avadominie@gmail.com>
2023-12-15 09:32:46 -0600

    Models set up

---
+++ Models/Walker.cs
@@ -0,0 +1,12 @@
+
+
+namespace Deshawns.Models;
+
+
+public class Walker
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CityId { get; set; }
+    public int DogId { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,160 @@
+
+using  Deshawns.Models;
+using  Deshawns.Models.DTOs;
+
+
+
+
+List<City> cities = new List<City> { };
+List<Dog> dogs = new List<Dog> { };
+List<Walker> walkers = new List<Walker> { };
+
+
+
+
+
+
+// Lists
+
+List<City> city = new List<City>()
+{
+    new City()
+    {
+        Id = 1,
+        Name = "Nashville",
+        WalkerId = 2,
+        DogId = 1
+    },
+    new City()
+    {
+        Id = 2,
+        Name = "New York",
+        WalkerId = 3,
+        DogId = 2
+    },
+    new City()
+    {
+        Id = 3,
+        Name = "Atlanta",
+        WalkerId = 4,
+        DogId = 3
+    },
+    new City()
+    {
+        Id = 4,
+        Name = "Chicago",
+        WalkerId = 5,
+        DogId = 4
+    },
+    new City()
+    {
+        Id = 5,
+        Name = "Birmingham",
+        WalkerId = 6,
+        DogId = 5
+    }
+};
+
+
+
+List<Dog> dog = new List<Dog>()
+{
+    new Dog()
+    {
+        Id = 1,
+        Name = "Buddy",
+        CityId = 2,
+        WalkerId = 1
+    },
+    new Dog()
+    {
+        Id = 2,
+        Name = "Charlie",
+        CityId = 2,
+        WalkerId = 2
+    },
+    new Dog()
+    {
+        Id = 3,
+        Name = "Boba",
+        CityId = 5,
+        WalkerId = 2
+    },
+    new Dog()
+    {
+        Id = 4,
+        Name = "Bailey",
+        CityId = 1,
+        WalkerId = 4
+    },
+    new Dog()
+    {
+        Id = 5,
+        Name = "Lucy",
+        CityId = 4,
+        WalkerId = 3
+    },
+    new Dog()
+    {
+        Id = 6,
+        Name = "Sadie",
+        CityId = 4,
+        WalkerId = 5
+    },
+    new Dog()
+    {
+        Id = 7,
+        Name = "Rocky",
+        CityId = 3,
+        WalkerId = 5
+    },
+};
+
+
+
+List<Walker> walker = new List<Walker>()
+{
+new Walker()
+    {
+        Id = 1,
+        Name = "John",
+        CityId = 1,
+        DogId = 1
+    },
+    new Walker()
+    {
+        Id = 2,
+        Name = "Emily",
+        CityId = 2,
+        DogId = 2
+    },
+    new Walker()
+    {
+        Id = 3,
+        Name = "David",
+        CityId = 3,
+        DogId = 3
+    },
+    new Walker()
+    {
+        Id = 4,
+        Name = "Sophie",
+        CityId = 4,
+        DogId = 4
+    },
+    new Walker()
+    {
+        Id = 5,
+        Name = "Alex",
+        CityId = 5,
+        DogId = 5
+    }
+};
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,10 +173,79 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+
+
+
+
+
+
+
+
+
 app.MapGet("/api/hello", () =>
 {
     return new { Message = "Welcome to DeShawn's Dog Walking" };
 });
+
+
+app.MapGet("/api/cities", () =>
+{
+    return city.Select(t => new CityDTO
+    {
+        
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.Run();
